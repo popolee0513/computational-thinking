96861cc7fbe8742e4dcdd8ee4d300bf78c0d2a3e
popo <B11000249@gmail.com>
2022-08-19 19:56:07 +0800

    Add files via upload

---
+++ Space Cows Transportation/ps1b.py
@@ -0,0 +1,80 @@
+###########################
+# 6.0002 Problem Set 1b: Space Change
+# Name:
+# Collaborators:
+# Time:
+# Author: charz, cdenise
+
+#================================
+# Part B: Golden Eggs
+#================================
+
+# Problem 1
+def dp_make_weight_greedy(egg_weights, target_weight, memo = {}):
+    """
+    Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
+    an infinite supply of eggs of each weight, and there is always a egg of value 1.
+    
+    Parameters:
+    egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
+    target_weight - int, amount of weight we want to find eggs to fit
+    memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
+    
+    Returns: int, smallest number of eggs needed to make target weight
+    """
+    # TODO: Your code here
+    egg_weights = sorted(list((egg_weights)),key=lambda item: -item)
+    
+    if len(egg_weights)==0:
+        return target_weight
+    else:
+        current = egg_weights[0]
+        
+        count = dp_make_weight(egg_weights[1:], target_weight%current, memo = {})
+        
+        result = (count+target_weight//current)
+
+        return result
+def dp_make_weight(egg_weights,target_weight,mini,memo={}): #, memo = {}
+    """
+    Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
+    an infinite supply of eggs of each weight, and there is always a egg of value 1.
+    
+    Parameters:
+    egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
+    target_weight - int, amount of weight we want to find eggs to fit
+    memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
+    
+    Returns: int, smallest number of eggs needed to make target weight
+    """
+    # TODO: Your code here
+    try:
+        ans = memo[target_weight] 
+        return ans
+    except:
+        if target_weight ==0:
+            return 0
+        elif target_weight< 0:
+            return 1e10
+        else :
+            for var in egg_weights:
+
+                count = dp_make_weight(egg_weights, target_weight-var,mini)
+                count += 1
+            
+                if count<=mini:
+                    mini = count
+                else:
+                    mini = mini
+            memo[target_weight] = mini        
+            return mini        
+
+# EXAMPLE TESTING CODE, feel free to add more if you'd like
+if __name__ == '__main__':
+    egg_weights = (1, 5, 10, 25)
+    n = 99
+    print("Egg weights = (1, 5, 10, 25)")
+    print("n = 99")
+    print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
+    print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
